daac5a13b1ab7b62d8c7d66aef8a654ccb8b2554
AfrazAhmad11 <afrazahmad1111@gmail.com>
2026-01-04 16:47:48 +0500

    day3 after a very long time

---
+++ day3_basicbot.py
@@ -0,0 +1,13 @@
+while True:
+    user_input = input("You: ").lower()
+    if user_input == "exit":
+        print("chatbot: Goodbye! (^_^)")
+        break
+    elif user_input == "help":
+        print("Chatbot: You can ask me about the weather, time, or just have a casual chat!")
+    elif user_input == "price":
+        print("Chatbot: Prices ranges from 10PKR to 1000PKR")
+    elif user_input == "contact":
+        print("Chatbot: Contacting you to our Agent")
+    else:
+        print("Chatbot: I don't understand")
