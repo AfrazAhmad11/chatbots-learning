b509afe9b8d37c072b69e430def5b6812c3d8c3c
AfrazAhmad11 <afrazahmad1111@gmail.com>
2026-01-04 17:36:41 +0500

    begginer level

--- day3_basicbot.py
+++ day3_basicbot.py
@@ -4,7 +4,7 @@ while True:
         print("chatbot: Goodbye! (^_^)")
         break
     elif user_input == "help":
-        print("Chatbot: You can ask me about the weather, time, or just have a casual chat!")
+        print("Chatbot: You can ask me about the price, help, contact or just have a casual chat!")
     elif user_input == "price":
         print("Chatbot: Prices ranges from 10PKR to 1000PKR")
     elif user_input == "contact":
